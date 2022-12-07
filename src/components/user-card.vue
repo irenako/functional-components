ae5cd9824f55eef5a86e146eaf846de3316a0375
irenako <i.irenakoval@gmail.com>
2022-12-07 13:50:21 +0200

    Finished Functional Components

--- src/components/user-card.vue
+++ src/components/user-card.vue
@@ -1,46 +1,43 @@
 <script>
-  export default {
-    functional: true,
-    render(h, context) {
-      const figureBlock = h(
-        'figure',
-        {
-          class: 'avatar',
-        },
-        [
-          h('img', {
-            attrs: {
-              src: context.props.user.avatar,
-              alt: context.props.user.name,
-            },
-          }),
-          h(
-            'button',
-            {
-              on: {
-                click(e) {
-                  e.stopPropagation();
-                  context.listeners.contact();
-                },
+export default {
+  functional: true,
+  render(h, context) {
+    const figureBlock = h(
+      'figure',
+      {
+        class: 'avatar',
+      },
+      [
+        h('img', {
+          attrs: {
+            src: context.props.user.avatar,
+            alt: context.props.user.name,
+          },
+        }),
+        h(
+          'button',
+          {
+            on: {
+              click(e) {
+                e.stopPropagation();
+                context.listeners.contact();
               },
             },
-            'Contact Me'
-          ),
-        ]
-      );
-
-      const additionalBlock = h('div', { class: 'additional' }, [figureBlock]);
-
-      const moreInfoBlock = h('div', { class: 'more-info' }, [
-        h('h1', context.props.user.name),
-        h('h3', context.props.user.role),
-        h('p', context.children),
-      ]);
-
-      return h('div', { class: 'card', on: context.listeners }, [
-        additionalBlock,
-        moreInfoBlock,
-      ]);
-    },
-  };
+          },
+          'Contact Me'
+        ),
+      ]
+    );
+    const additionalBlock = h('div', { class: 'additional' }, [figureBlock]);
+    const moreInfoBlock = h('div', { class: 'more-info' }, [
+      h('h1', context.props.user.name),
+      h('h3', context.props.user.role),
+      h('p', context.children),
+    ]);
+    return h('div', { class: 'card', on: context.listeners }, [
+      additionalBlock,
+      moreInfoBlock,
+    ]);
+  },
+};
 </script>
